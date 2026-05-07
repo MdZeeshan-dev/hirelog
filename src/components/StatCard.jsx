d1de55dc042f8942be158bc077deb018ed17c043
Md Zeeshan <zeeshan.codev@gmail.com>
2026-05-07 19:52:06 +0530

    nh

--- src/components/StatCard.jsx
+++ src/components/StatCard.jsx
@@ -1,5 +1,13 @@
 import React from 'react'
+import React from 'react'
 
+const StatCard = () => {
+  return (
+    <div>StatCard</div>
+  )
+}
+
+export default StatCard
 const StatCard = () => {
   return (
     <div>StatCard</div>
